2a2bdff9aefb69d9f70b58315487723be19477b6
chrzyki <christoph@functor-argument.net>
2019-11-11 10:34:13 +0100

    Code cleanup, added tests, release prep

--- lexibank_suntb.py
+++ lexibank_suntb.py
@@ -1,24 +1,21 @@
 from pathlib import Path
-from pylexibank.dataset import Dataset as BaseDataset
-from pylexibank import Language, Concept, Lexeme
 
-from pylexibank.util import getEvoBibAsBibtex
-from pylexibank import progressbar
-from pylexibank import FormSpec
-
-from clldutils.misc import slug
-from clldutils.text import *
-from clldutils.text import split_text
-
-from pyclts import TranscriptionSystem
 import attr
+from clldutils.misc import slug
+from pylexibank import FormSpec
+from pylexibank import Language, Concept
+from pylexibank import progressbar
+from pylexibank.dataset import Dataset as BaseDataset
+from pylexibank.util import getEvoBibAsBibtex
+
 
 @attr.s
 class CustomLanguage(Language):
     Latitude = attr.ib(default=None)
     Longitude = attr.ib(default=None)
     SubGroup = attr.ib(default=None)
-    Family = attr.ib(default='Sino-Tibetan')
+    Family = attr.ib(default="Sino-Tibetan")
+
 
 @attr.s
 class CustomConcept(Concept):
@@ -32,10 +29,8 @@ class Dataset(BaseDataset):
     language_class = CustomLanguage
     concept_class = CustomConcept
     form_spec = FormSpec(
-            separators = ';,/',
-            missing_data = ('*', '---', '-', '--'),
-            brackets = {'[': ']', '(': ')'}
-            )
+        separators=";,/", missing_data=("*", "---", "-", "--"), brackets={"[": "]", "(": ")"}
+    )
 
     def cmd_download(self, args):
         self.raw_dir.write("sources.bib", getEvoBibAsBibtex("Sun1991"))
@@ -43,29 +38,25 @@ class Dataset(BaseDataset):
     def cmd_makecldf(self, args):
 
         args.writer.add_sources()
-        language_lookup = args.writer.add_languages(
-                lookup_factory = 'Name')
+        language_lookup = args.writer.add_languages(lookup_factory="Name")
 
         concept_lookup = {}
-        for concept in self.conceptlist.concepts.values():
-            idx = concept.id.split('-')[-1]+'_'+slug(concept.english)
+        for concept in self.conceptlists[0].concepts.values():
+            idx = concept.id.split("-")[-1] + "_" + slug(concept.english)
             concept_lookup[concept.english] = idx
             concept_lookup[concept.number] = idx
             args.writer.add_concept(
-                    ID=idx,
-                    Concepticon_ID=concept.concepticon_id,
-                    Concepticon_Gloss=concept.concepticon_gloss,
-                    Name=concept.english,
-                    Chinese_Gloss=concept.attributes['chinese']
-                    )
-        for entry in progressbar(
-                self.raw_dir.read_csv("ZMYYC.csv", delimiter="\t", dicts=True)):
+                ID=idx,
+                Concepticon_ID=concept.concepticon_id,
+                Concepticon_Gloss=concept.concepticon_gloss,
+                Name=concept.english,
+                Chinese_Gloss=concept.attributes["chinese"],
+            )
+        for entry in progressbar(self.raw_dir.read_csv("ZMYYC.csv", delimiter="\t", dicts=True)):
             args.writer.add_forms_from_value(
-                    Language_ID=language_lookup[entry['language']],
-                    Parameter_ID=concept_lookup.get(
-                        entry['srcid'].split('.')[0]),
-                    Local_ID=entry['rn'],
-                    Value=entry['reflex'],
-                    Source=['Sun1991']
-                    )
-
+                Language_ID=language_lookup[entry["language"]],
+                Parameter_ID=concept_lookup.get(entry["srcid"].split(".")[0]),
+                Local_ID=entry["rn"],
+                Value=entry["reflex"],
+                Source=["Sun1991"],
+            )
--- setup.py
+++ setup.py
@@ -2,31 +2,19 @@ from setuptools import setup
 import json
 
 
-with open('metadata.json') as fp:
+with open("metadata.json") as fp:
     metadata = json.load(fp)
 
 
 setup(
-    name='lexibank_suntb',
-    description=metadata['title'],
-    license=metadata.get('license', ''),
-    url=metadata.get('url', ''),
-    py_modules=['lexibank_suntb'],
+    name="lexibank_suntb",
+    description=metadata["title"],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
+    py_modules=["lexibank_suntb"],
     include_package_data=True,
     zip_safe=False,
-    entry_points={
-        'lexibank.dataset': [
-            'suntb=lexibank_suntb:Dataset',
-        ]
-    },
-    install_requires=[
-        'pylexibank>=2.0.0',
-        'segments>=2.0.2'
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf',
-        ],
-    },
+    entry_points={"lexibank.dataset": ["suntb=lexibank_suntb:Dataset"]},
+    install_requires=["pylexibank>=2.1", "segments>=2.0.2"],
+    extras_require={"test": ["pytest-cldf"]},
 )
-
